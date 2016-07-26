b07e0cc9f66f0db415b010c8157afadde980ba3a
André Luiz Leoni <andreluizleoni@gmail.com>
2016-07-26 10:02:38 -0300

    PHP - excluirminhacontaController
    Controller que chama o view “excluirminhaconta.php” para excluir a
    conta do usuário. A conta exclui automaticamente todos os registros
    também da conta de maneira irreversível.

---
+++ controllers/excluirminhacontaController.php
@@ -0,0 +1,21 @@
+<?php
+
+class excluirminhacontaController extends controller {
+	
+	function __construct() {
+		parent::__construct();
+	}
+
+	public function index(){
+		$dados = array();
+
+		$this->loadTemplate("excluirminhaconta", $dados);
+	}
+
+	public function excluirConta(){
+		$usuario = new usuario();
+		$usuario->excluirConta();
+		session_destroy();
+		$this->loadView("loginview", $dados);
+	}
+}
