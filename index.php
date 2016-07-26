462c738b9ca73855faab39babb686fc9365e8aef
André Luiz Leoni <andreluizleoni@gmail.com>
2016-07-26 10:47:59 -0300

    PHP - index.php
    Arquivo índex controlador de todo o sistema, é ele que executa a função
    de autoload para juntar todas as funções do sistema e fazer o
    carregamento delas para que o sistema funcione com a estrutura MVC
    conforme proposto. Por padrão ele executa a função dentro do core
    RUN(), que é a função que chama as paginas da web conforme o url
    solicitado.

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+require 'config.php';
+$_SESSION['status'] = '';
+date_default_timezone_set('America/Sao_Paulo');
+
+
+spl_autoload_register(function ($class){
+	if(strpos($class, 'Controller') > -1) {
+		if(file_exists('controllers/'.$class.'.php')) {
+			require_once 'controllers/'.$class.'.php';
+		} else {
+			require_once 'views/paginainvalida.php';
+		}
+	} else if(file_exists('models/'.$class.'.php')) {
+		require_once 'models/'.$class.'.php';
+	} else if(file_exists('core/'.$class.'.php')) {
+		require_once 'core/'.$class.'.php';
+	} else {
+		require_once 'views/paginainvalida.php';
+	}
+});
+
+$core = new Core();
+$core->run();
+?>
