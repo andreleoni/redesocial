ec5989b6cfb1a96809b7fdadc740183f73327a81
André Luiz Leoni <andreluizleoni@gmail.com>
2016-07-26 11:02:05 -0300

    PHP - View - excluirminhaconta.php
    View de exclusão de conta, com um sim e um não para exclusão.

---
+++ views/excluirminhaconta.php
@@ -0,0 +1,8 @@
+<center><h3> Você tem certeza que deseja excluir a sua conta? </h3></center>
+</br>
+<center> <h4> Os dados excluidos de conta serão irreversíveis, deseja prosseguir com a exclusão? </h4> </center>
+</br>
+<center>
+	<a href="/excluirminhaconta/excluirConta"> <input type="button" value="Sim" class="btn btn-success" /> </a>
+	<a href="/meuperfil"> <input type="button" value="Não" class="btn btn-danger" /> </a>
+</center>
